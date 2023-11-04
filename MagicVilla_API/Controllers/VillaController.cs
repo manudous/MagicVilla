e8662ecc435d0bff7b7a04f0f84d6d5a9f0b707f
manudous <manudous@gmail.com>
2023-11-04 22:24:13 +0100

    add patch endpoint

--- MagicVilla_API/Controllers/VillaController.cs
+++ MagicVilla_API/Controllers/VillaController.cs
@@ -1,6 +1,7 @@
 ﻿using MagicVilla_API.Datos;
 using MagicVilla_API.Modelos.DTO;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.JsonPatch;
 using Microsoft.AspNetCore.Mvc;
 
 namespace MagicVilla_API.Controllers
@@ -13,16 +14,16 @@ namespace MagicVilla_API.Controllers
         [ProducesResponseType(StatusCodes.Status200OK)]
         public ActionResult<IEnumerable<VillaDto>> GetVillas()
         {
-           return Ok(VillaStore.villaList);
+            return Ok(VillaStore.villaList);
         }
 
-        [HttpGet("id:int", Name = "GetVilla")]
+        [HttpGet("{id:int}", Name = "GetVilla")]
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
         public ActionResult<VillaDto> GetVilla(int id)
         {
-            if(id == 0)
+            if (id == 0)
             {
                 return BadRequest();
             }
@@ -30,7 +31,7 @@ namespace MagicVilla_API.Controllers
 
             if (villa == null)
             {
-                   return NotFound();
+                return NotFound();
             }
             return Ok(villa);
         }
@@ -64,10 +65,10 @@ namespace MagicVilla_API.Controllers
             villaDto.Id = VillaStore.villaList.OrderByDescending(v => v.Id).FirstOrDefault()?.Id + 1 ?? 1;
             VillaStore.villaList.Add(villaDto);
 
-            return CreatedAtRoute("GetVilla", new {id = villaDto.Id, villaDto});
+            return CreatedAtRoute("GetVilla", new { id = villaDto.Id, villaDto });
         }
 
-        [HttpDelete("id:int")]
+        [HttpDelete("{id:int}")]
         [ProducesResponseType(StatusCodes.Status204NoContent)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
@@ -88,6 +89,62 @@ namespace MagicVilla_API.Controllers
             VillaStore.villaList.Remove(villa);
 
             return NoContent();
+        }
+
+        [HttpPut("{id:int}")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public IActionResult UpdateVilla(int id, [FromBody] VillaDto villaDto)
+        {
+            if (villaDto == null || id != villaDto.Id)
+            {
+                return BadRequest();
+            }
+            var villa = VillaStore.villaList.FirstOrDefault(v => v.Id == id);
+
+            if (villa == null)
+            {
+                return NotFound();
+            }
+            villa.Nombre = villaDto.Nombre;
+            villa.Ocupantes = villaDto.Ocupantes;
+            villa.MetrosCuadrados = villaDto.MetrosCuadrados;
+
+            return NoContent();
+        }
+
+        [HttpPatch("{id:int}")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public IActionResult UpdatePartialVilla(int id, JsonPatchDocument<VillaDto> patchDto)
+        {
+            if (patchDto == null || id == 0)
+            {
+                return BadRequest();
+            }
+            var villa = VillaStore.villaList.FirstOrDefault(v => v.Id == id);
+
+            if (villa == null)
+            {
+                return NotFound();
+            }   
+
+            patchDto.ApplyTo(villa, ModelState);
+
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            return NoContent();
+
+            //[
+            //    {
+            //        "path": "/nombre",
+            //        "op": "replace",
+            //        "value": "Nueva Villa"
+            //    }
+            //]
         }
 
     }
--- MagicVilla_API/Modelos/DTO/VillaDto.cs
+++ MagicVilla_API/Modelos/DTO/VillaDto.cs
@@ -9,5 +9,8 @@ namespace MagicVilla_API.Modelos.DTO
         [Required(ErrorMessage = "El nombre de la villa es requerido")]
         [MaxLength(30, ErrorMessage = "El nombre de la villa no puede ser mayor a 30 caracteres")]
         public string Nombre { get; set; } = string.Empty;
+
+        public int Ocupantes { get; set; }
+        public int MetrosCuadrados { get; set; }
     }
 }
